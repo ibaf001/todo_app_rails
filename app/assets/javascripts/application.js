1ae16b27403f317b06675e7ae2cdea07f890267b
ibo <ibo.b@tcs.com>
2016-06-03 13:39:17 -0400

    end tutorial with complete bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,14 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
+$(document).ready(function() {
+  $('.has-tooltip').tooltip();
+});
+
+$(document).ready(function() {
+  $('.has-tooltip').tooltip();
+  $('.has-popover').popover({
+    trigger: 'hover'
+  });
+});
+
