3829451f349017f948fbe8013036b7a486fc1365
ibo <ibo.b@tcs.com>
2016-06-03 11:17:22 -0400

    First commit for app that uses bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoApp::Application.config.secret_key_base = 'd10328c6d3ea56a952b053f268de82c416b460ee2690bd6acd72edd5d29fe70b0d89dd2cd3aa96477b7a495e67e34bcf4ffa09ca08ec39ac3da5cac118f86682'
